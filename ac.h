7f9c22772e2ac2a624a738ccc0b74b0d6af89f1d
happywowwow <pikeszfish@gmail.com>
2014-06-12 10:44:32 +0800

    add ac.h

---
+++ ac.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_LEN 256;
+#define PATTERN_SIG1 0
+#define PATTERN_SIG2 1
+#define PATTERN_SIG3 2
+typedef struct
+{    
+     /* data */
+}ac_c;
+typedef dtruct{
+	char pattern[MAX_LEN];
+	char name[MAX_LEN];
+	int type;
+	int enabled;
+}ac_pattern;
